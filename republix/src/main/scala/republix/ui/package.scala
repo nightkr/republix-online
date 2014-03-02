735971d16d87638c0ddbfbb2acb2708948a939dd
Carl Andreas Dybdahl <tailcalled@gmail.com>
2014-03-02 19:33:44 +0100

    I made you a Graphical GUI User Interface

--- republix/src/main/scala/republix/ui/package.scala
+++ republix/src/main/scala/republix/ui/package.scala
@@ -31,18 +31,11 @@ package object ui {
 	def debug(comp: JComponent): Unit = {
 		comp.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3))
 	}
-
-	def layoutify(comp: JComponent): Unit = {
-		comp.setLayout(new GridBagLayout())
-	}
-	def place(parent: JComponent, child: JComponent, gx: Int, gy: Int, gw: Int = 1, gh: Int = 1): Unit = {
-		val gbc = new GridBagConstraints()
-		gbc.gridx = gx
-		gbc.gridy = gy
-		gbc.gridwidth = gw
-		gbc.gridheight = gh
-		gbc.weightx = 1.0
-		parent.add(child, gbc)
+	def groupLayout(owner: JComponent): GroupLayout = {
+		val layout = new GroupLayout(owner)
+		layout.setAutoCreateGaps(true)
+		layout.setAutoCreateContainerGaps(true)
+		layout
 	}
 
 }
