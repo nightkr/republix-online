f2cc3ccd28dd2c01e58d85925c5102e20a39f8f8
Nullable <teo@nullable.se>
2014-03-02 15:40:57 +0100

    Started writing simulation model

---
+++ republix/src/main/scala/republix/sim/Model.scala
@@ -0,0 +1,32 @@
+package republix.sim
+
+import Node.Intensity
+
+trait Model {
+	val nodes: Seq[Node]
+}
+
+sealed trait Effect
+
+sealed trait Node {
+	val model: Model
+	val causes: Map[Node, Effect]
+
+	lazy val effects: Map[Node, Effect] = (for {
+		node <- model.nodes
+		(otherNode, effect) <- node.causes
+		if otherNode == this
+	} yield (node, effect)).toMap
+}
+
+object Node {
+	type Intensity = Int
+}
+
+case class Status(model: Model, causes: Map[Node, Effect]) extends Node
+
+case class Law(model: Model) extends Node {
+	override val causes: Map[Node, Effect] = Map()
+}
+
+case class Event(model: Model, causes: Map[Node, Effect], startTrigger: Intensity, stopTrigger: Intensity) extends Node
