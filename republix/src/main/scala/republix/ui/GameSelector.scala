5378fbbad78dfc11d2c33ee0f1b1e7737e7b3c71
Teo Klestrup Röijezon <teozkr@gmail.com>
2014-03-02 18:41:15 +0100

    Copyright headers are super important!

--- republix/src/main/scala/republix/ui/GameSelector.scala
+++ republix/src/main/scala/republix/ui/GameSelector.scala
@@ -1,3 +1,22 @@
+/*
+ * Copyright © 2014 Teo Klestrup, Carl Dybdahl
+ *
+ * This file is part of Republix.
+ *
+ * Republix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Republix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Republix.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package republix.ui
 
 import javax.swing._
@@ -7,12 +26,15 @@ class GameSelector(parent: RepublixNav) extends JPanel {
 	object IP extends JTextField {
 		setMinimumSize(getPreferredSize)
 	}
+
 	object Port extends JTextField {
 		setMinimumSize(getPreferredSize)
 	}
+
 	object Ok extends JButton("Ok") {
-		
+
 	}
+
 	object Cancel extends JButton("Cancel") {
 		addActionListener(on {
 			parent.menu()
@@ -21,9 +43,9 @@ class GameSelector(parent: RepublixNav) extends JPanel {
 
 	layoutify(this)
 	place(this, new JLabel("Ip Address:"), 0, 0)
-	place(this, IP,                        1, 0)
-	place(this, new JLabel("Port:"),       0, 1)
-	place(this, Port,                      1, 1)
-	place(this, Cancel,                    0, 2)
-	place(this, Ok,                        1, 2)
+	place(this, IP, 1, 0)
+	place(this, new JLabel("Port:"), 0, 1)
+	place(this, Port, 1, 1)
+	place(this, Cancel, 0, 2)
+	place(this, Ok, 1, 2)
 }
--- republix/src/main/scala/republix/ui/GameSetup.scala
+++ republix/src/main/scala/republix/ui/GameSetup.scala
@@ -1,3 +1,22 @@
+/*
+ * Copyright © 2014 Teo Klestrup, Carl Dybdahl
+ *
+ * This file is part of Republix.
+ *
+ * Republix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Republix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Republix.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package republix.ui
 
 import javax.swing._
@@ -7,9 +26,11 @@ class GameSetup(parent: RepublixNav) extends JPanel {
 	object Port extends JTextField {
 
 	}
+
 	object Ok extends JButton("Ok") {
-		
+
 	}
+
 	object Cancel extends JButton("Cancel") {
 		addActionListener(on {
 			parent.menu()
@@ -17,9 +38,9 @@ class GameSetup(parent: RepublixNav) extends JPanel {
 	}
 
 	layoutify(this)
-	place(this, new JLabel("Port:"),       0, 0)
-	place(this, Port,                      1, 0)
-	place(this, Cancel,                    0, 1)
-	place(this, Ok,                        1, 1)
+	place(this, new JLabel("Port:"), 0, 0)
+	place(this, Port, 1, 0)
+	place(this, Cancel, 0, 1)
+	place(this, Ok, 1, 1)
 
 }
